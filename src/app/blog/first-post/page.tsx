73cfab481cfa0c02a6df8a53448fd74d2eaffad3
Bridget <bgl2126@columbia.edu>
2025-06-12 21:21:08 -0400

    fixed apostrophes

--- src/app/blog/first-post/page.tsx
+++ src/app/blog/first-post/page.tsx
@@ -8,45 +8,45 @@ export default function FirstPost() {
       </p>
 
       <p className="mb-4">
-        For those of you who have never heard of Neo before, it's one of the world's most prestigious accelerator programs for college students doing CS (aka don't shower) & are interested in startups. Ever heard of Cursor, that one company now taking every single entry-level CS major's job? Neo launched them off the ground. Their acceptance rate is less than Harvard and Yale's, so this is going to be a pretty tough journey.
+        For those of you who have never heard of Neo before, it&apos;s one of the world&apos;s most prestigious accelerator programs for college students doing CS (aka don&apos;t shower) & are interested in startups. Ever heard of Cursor, that one company now taking every single entry-level CS major&apos;s job? Neo launched them off the ground. Their acceptance rate is less than Harvard and Yale&apos;s, so this is going to be a pretty tough journey.
       </p>
 
       <p className="mb-4">
-        Of course, I have a variety of other goals, including subquests and adventures and whatnot—become a really fucking good protein/small molecule ML researcher, start filming videos/publicize my street photography, meet 100 more interesting people—this summer, but that's my one hard goal.
+        Of course, I have a variety of other goals, including subquests and adventures and whatnot—become a really fucking good protein/small molecule ML researcher, start filming videos/publicize my street photography, meet 100 more interesting people—this summer, but that&apos;s my one hard goal.
       </p>
 
       <p className="mb-4">
-        And there's a deadline, too: Jun 22, 2025. I made a promise to someone that I'd find my internal conviction this summer, and this is my response.
+        And there&apos;s a deadline, too: Jun 22, 2025. I made a promise to someone that I&apos;d find my internal conviction this summer, and this is my response.
       </p>
 
       <p className="mb-4">
-        A quick caveat here—what even is internal conviction? It's a quality that defines you, shapes your entire life's motivations, and yet is probably only found in less than one percent of the population. It's knowing exactly who you are, what your goals are, and having the ambition and sheer determination to fulfill all of that. Highly successful people, in my opinion, all have this quality. Anyways, back to the point of this all.
+        A quick caveat here—what even is internal conviction? It&apos;s a quality that defines you, shapes your entire life&apos;s motivations, and yet is probably only found in less than one percent of the population. It&apos;s knowing exactly who you are, what your goals are, and having the ambition and sheer determination to fulfill all of that. Highly successful people, in my opinion, all have this quality. Anyways, back to the point of this all.
       </p>
 
       <p className="mb-4">
-        I want to live up to that person's promise. As an 18 year old Asian girl with a high-pitched voice who shifted track from pre-med to CS only a year ago, I meet a lot of people who question my abilities; honestly, sometimes I question myself. Why do I want to be a founder? What qualifications do I have when there are people out there who have been coding before they could talk? Why am I even in this space? So here's my overly publicized story and reason why.
+        I want to live up to that person&apos;s promise. As an 18 year old Asian girl with a high-pitched voice who shifted track from pre-med to CS only a year ago, I meet a lot of people who question my abilities; honestly, sometimes I question myself. Why do I want to be a founder? What qualifications do I have when there are people out there who have been coding before they could talk? Why am I even in this space? So here&apos;s my overly publicized story and reason why.
       </p>
 
       <p className="mb-4">—</p>
 
       <p className="mb-4">
-        Okay, so that's the cold intro part of this (another caveat: when you've cold intro-d yourself to 50 people and they all ignore you does that make it an ice-d intro? Yeah that was funnier in my head). Here's the warm intro part. Or, at least warmer in the sense that I will now proceed to overshare my life story because it's relevant.
+        Okay, so that&apos;s the cold intro part of this (another caveat: when you&apos;ve cold intro-d yourself to 50 people and they all ignore you does that make it an ice-d intro? Yeah that was funnier in my head). Here&apos;s the warm intro part. Or, at least warmer in the sense that I will now proceed to overshare my life story because it&apos;s relevant.
       </p>
 
       <p className="mb-4">
-        I'm from the Bay Area, and yes, the stereotypes are true: it's competitive, high schoolers are depressed, and everyone and their mom is in CS. I joined the Njoo Lab, an organic chemistry group synthesizing a shit ton of small molecules, and fell in love with biochemistry at age 14. To be very clear: there were parts of it I hated—wet lab takes hours, reactions rarely work, I'm probably getting cancer because of all the carcinogenic fumes I've inhaled, and I've lost one too many pairs of shoes to toxic chemical spills. But what I loved about it was the creative and troubleshooting processes. As my previous mentor used to say, organic chemistry is the boot code of medicine, and it's all about problem solving with implications of creating drugs that save lives. Yet, the more I delved into chemistry, the more problems I saw.
+        I&apos;m from the Bay Area, and yes, the stereotypes are true: it&apos;s competitive, high schoolers are depressed, and everyone and their mom is in CS. I joined the Njoo Lab, an organic chemistry group synthesizing a shit ton of small molecules, and fell in love with biochemistry at age 14. To be very clear: there were parts of it I hated—wet lab takes hours, reactions rarely work, I&apos;m probably getting cancer because of all the carcinogenic fumes I&apos;ve inhaled, and I&apos;ve lost one too many pairs of shoes to toxic chemical spills. But what I loved about it was the creative and troubleshooting processes. As my previous mentor used to say, organic chemistry is the boot code of medicine, and it&apos;s all about problem solving with implications of creating drugs that save lives. Yet, the more I delved into chemistry, the more problems I saw.
       </p>
 
       <p className="mb-4">
-        Traditionally, the drug discovery pipeline takes years (up to a decade) and millions of dollars in investment—all for one or two drug candidates in FDA Trial 3 or 4 to potentially be screw-ups because the human body doesn't absorb them well or some random side effect shows up. It's inefficient and drawn out.
+        Traditionally, the drug discovery pipeline takes years (up to a decade) and millions of dollars in investment—all for one or two drug candidates in FDA Trial 3 or 4 to potentially be screw-ups because the human body doesn&apos;t absorb them well or some random side effect shows up. It&apos;s inefficient and drawn out.
       </p>
 
       <p className="mb-4">
-        The potential for computational work here is enormous. We could reduce this timeline by years, and that's what amazed me when I ran some initial docking experiments (using a computer to simulate the interaction between your target ligand, small molecules in this case, and your protein). And when AlphaFold 2 came out, that amazement spiraled. Researchers literally dedicate their lives to decoding a protein's structure. Millions of dollars are spent on experiments. AlphaFold accomplishes this in less than 30 seconds.
+        The potential for computational work here is enormous. We could reduce this timeline by years, and that&apos;s what amazed me when I ran some initial docking experiments (using a computer to simulate the interaction between your target ligand, small molecules in this case, and your protein). And when AlphaFold 2 came out, that amazement spiraled. Researchers literally dedicate their lives to decoding a protein&apos;s structure. Millions of dollars are spent on experiments. AlphaFold accomplishes this in less than 30 seconds.
       </p>
 
       <p className="mb-4">
-        And ever since then, I've been fascinated by ML. So that's my story—I want to go one step further: not just protein prediction, but generation.
+        And ever since then, I&apos;ve been fascinated by ML. So that&apos;s my story—I want to go one step further: not just protein prediction, but generation.
       </p>
 
       <p className="mb-4">
