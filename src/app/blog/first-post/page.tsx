564a202b36412f1d848c686df9cd6bbe4c35b4ac
Bridget <bgl2126@columbia.edu>
2025-07-08 18:35:58 -0400

    fix

--- src/app/blog/first-post/page.tsx
+++ src/app/blog/first-post/page.tsx
@@ -8,53 +8,12 @@ export default function FirstPost() {
       
       <div className="prose lg:prose-xl">
         <p className="text-neutral-700 mb-6">
-          Hi, my name is Bridget and I have one hard goal this summer: become a Neo Scholar.
+          placeholder
         </p>
-
-        <p className="mb-6">
-          For those of you who have never heard of Neo before, it&apos;s one of the world&apos;s most prestigious accelerator programs for college students doing CS (aka don&apos;t shower) & are interested in startups. Ever heard of Cursor, that one company now taking every single entry-level CS major&apos;s job? Neo launched them off the ground. Their acceptance rate is less than Harvard and Yale&apos;s, so this is going to be a pretty tough journey.
+        <p className="text-neutral-700 mb-6">
+          
         </p>
-
-        <p className="mb-6">
-          Of course, I have a variety of other goals, including subquests and adventures and whatnot—become a really fucking good protein/small molecule ML researcher, start filming videos/publicize my street photography, meet 100 more interesting people—this summer, but that&apos;s my one hard goal.
-        </p>
-
-        <p className="mb-6">
-          And there&apos;s a deadline, too: July 7, 2025. I made a promise to someone that I&apos;d find my internal conviction this summer, and this is my response.
-        </p>
-
-        <p className="mb-6">
-          A quick caveat here—what even is internal conviction? It&apos;s a quality that defines you, shapes your entire life&apos;s motivations, and yet is probably only found in less than one percent of the population. It&apos;s knowing exactly who you are, what your goals are, and having the ambition and sheer determination to fulfill all of that. Highly successful people, in my opinion, all have this quality. Anyways, back to the point of this all.
-        </p>
-
-        <p className="mb-6">
-          I want to live up to that person&apos;s promise. As an 18 year old Asian girl with a high-pitched voice who shifted track from pre-med to CS only a year ago, I meet a lot of people who question my abilities; honestly, sometimes I question myself. Why do I want to be a founder? What qualifications do I have when there are people out there who have been coding before they could talk? Why am I even in this space? So here&apos;s my overly publicized story and reason why.
-        </p>
-
-        <p className="mb-6">—</p>
-
-        <p className="mb-6">
-          Okay, so that&apos;s the cold intro part of this. Here&apos;s the warm intro part. Or, at least warmer in the sense that I will now proceed to overshare my life story because it&apos;s relevant.
-        </p>
-
-        <p className="mb-6">
-          I&apos;m from the Bay Area, and yes, the stereotypes are true: it&apos;s competitive, high schoolers are depressed, and everyone and their mom is in CS. I joined the Njoo Lab, an organic chemistry group synthesizing a shit ton of small molecules, and fell in love with biochemistry at age 14. To be very clear: there were parts of it I hated—wet lab takes hours, reactions rarely work, I&apos;m probably getting cancer because of all the carcinogenic fumes I&apos;ve inhaled, and I&apos;ve lost one too many pairs of shoes to toxic chemical spills. But what I loved about it was the creative and troubleshooting processes. As my previous mentor used to say, organic chemistry is the boot code of medicine, and it&apos;s all about problem solving with implications of creating drugs that save lives. Yet, the more I delved into experimental chemistry, the more problems I saw.
-        </p>
-
-        <p className="mb-6">
-          Traditionally, the drug discovery pipeline takes years (up to a decade) and millions of dollars in investment—all for one or two drug candidates in FDA Trial 3 or 4 to potentially be screw-ups because the human body doesn&apos;t absorb them well or some random side effect shows up. It&apos;s inefficient and drawn out.
-        </p>
-
-        <p className="mb-6">
-          The potential for computational work here is enormous. We can reduce this timeline by years. That&apos;s precisely what amazed me when I ran some initial docking experiments (using a computer to simulate the interaction between your target ligand, small molecules in this case, and your protein). And when AlphaFold 2 came out, that amazement spiraled. Researchers literally dedicate their lives to decoding a protein&apos;s structure. Millions of dollars are spent on experiments. AlphaFold accomplishes this in less than 30 seconds.
-        </p>
-
-        <p className="mb-6">
-          And ever since then, I&apos;ve been fascinated by ML. So that&apos;s my story—I want to go one step further: not just protein prediction, but generation.
-        </p>
-
-        <p className="mb-6">
-          My dream is to create a company that uses ML to generate new proteins, antibodies, drugs, and therapeutics.
+        <p className="text-neutral-700">
         </p>
       </div>
     </article>
